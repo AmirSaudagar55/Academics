402d608b9ac236c8195f9ff20eb76af00769fb0b
Amir Saudagar <121443390+AmirSaudagar55@users.noreply.github.com>
2022-12-27 13:40:08 +0530

    Update Ass1_oop.cpp

--- OOP AND CG/Oop/Ass1_oop.cpp
+++ OOP AND CG/Oop/Ass1_oop.cpp
@@ -1,9 +1,10 @@
-Implement a class Complex which represents the Complex Number data type. Implement the
+/*Implement a class Complex which represents the Complex Number data type. Implement the
 following operations:
 1. Constructor (including a default constructor which creates the complex number 0+0i).
 2. Overloaded operator+ to add two complex numbers.
 3. Overloaded operator* to multiply two complex numbers.
 4. Overloaded << and >> to print and read Complex Numbers.
+*/
 
 #include<iostream>
 using namespace std;
@@ -34,7 +35,7 @@ complex complex::operator *(complex obj)
 {
     complex temp;
     temp.real=(real*obj.real)-(img*obj.img);
-    temp.img=(real*obj.img)+(img+obj.img);
+    temp.img=(real*obj.img)+(img*obj.real);
     return (temp);
 }
 

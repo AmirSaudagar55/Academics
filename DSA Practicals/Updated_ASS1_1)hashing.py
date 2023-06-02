d9d506a9c881f5cc7bc442748642ba511c18c509
Amir Saudagar <121443390+AmirSaudagar55@users.noreply.github.com>
2023-06-02 07:17:53 +0530

    Update Updated_ASS1_1)hashing.py
    This includes proper implementation of the phone directory which uses two collision resolution techniques i.e. Linear Probing and Separate Chaining as mentioned in the Statement of Assignment.

--- DSA Practicals/Updated_ASS1_1)hashing.py
+++ DSA Practicals/Updated_ASS1_1)hashing.py
@@ -1,109 +1,110 @@
-clients = [None] * 10  # Directory to store client information
-tab_size = len(clients)  # Size of the directory
+class PhoneDirectory:
+    def __init__(self):
+        self.MAX = 10
+        self.arrSeperate = [[] for i in range(self.MAX)]
+        self.arrLinear = [None for i in range(self.MAX)]
+    
+    def get_hash(self, key):
+        hash = 0
+        for char in key:
+            hash += ord(char)
+        return hash % self.MAX
 
-def hash_function(x):
-    return sum(ord(char) for char in x) % tab_size  # Simple hash function based on character ASCII values
 
-def insert_linear_probing(name, number):
-    global clients, tab_size
-    index = hash_function(name)  # Hash Function
+    #--------------------------------------------Separate Chaining--------------------------------------------------------------    
+    def setSeparate(self, key, value):
+        h = self.get_hash(key)
+        found = False
+        
+        for index,element in enumerate(self.arrSeperate):
+            if(len(element)==2 and element[0] == key):
+                self.arrSeperate[h][index] = (key,value)
+                found = True
+                break
+            
+        if not found:
+            self.arrSeperate[h].append((key,value))
+    
+    def lookSeparate(self, key):
+        comparison = 0
+        h = self.get_hash(key)
+        for element in self.arrSeperate[h]:
+            comparison += 1
+            if(element[0] == key):
+                print("Comparison Required : ",comparison)
+                return element[1]
+            
 
-    while clients[index] is not None:
-        index = (index + 1) % tab_size  # Linear Probing
-
-    clients[index] = (name, number)
-    print("Data inserted successfully using Linear Probing.")
-
-def insert_quadratic_probing(name, number):
-    global clients, tab_size
-    index = hash_function(name)  # Hash Function
-    offset = 1
-
-    while clients[index] is not None:
-        index = (index + offset**2) % tab_size  # Quadratic Probing
-        offset += 1
-
-    clients[index] = (name, number)
-    print("Data inserted successfully using Quadratic Probing.")
-
-def look_up_linear_probing(x):
-    global clients, tab_size
-    index = hash_function(x)  # Hash Function
-    comparisons = 0
-
-    while clients[index] is not None:
-        comparisons += 1
-        if clients[index][0] == x:
-            print(f"{x} client has telephone number as: {clients[index][1]}")
+    #--------------------------------LINEAR PROBING--------------------------------------------------------------
+    def setLinear(self, key, value):
+        index = self.get_hash(key)
+        # Linear probing to find the next available slot
+        while self.arrLinear[index] is not None:
+            # If the key already exists, update the value
+            if self.arrLinear[index][0] == key:
+                self.arrLinear[index] = (key, value)
+                return
+            index = (index + 1) % self.MAX
+        
+        # Insert the key-value pair at the available slot
+        self.arrLinear[index] = (key, value)
+    
+    def lookLinear(self, key):
+        comparison = 0
+        index = self.get_hash(key)
+        
+        while self.arrLinear[index] is not None:
+            comparison += 1
+            if self.arrLinear[index][0] == key:
+                print("Comparison required : ",comparison)
+                return self.arrLinear[index][1]
+            index = (index + 1) % self.MAX
+        
+        # Key not found
+        return None
+    
+def main():
+    d1 = PhoneDirectory()
+    
+    while(True):
+        print("\n1.Add Contact using Separate Chainging")
+        print("\n2.Add Contact using Linear Probing")
+        print("\n3.Display Contact using Separate Chaining")
+        print("\n4.Display Contact using Linear Probing")
+        print("\n5.Exit")
+        choice = int(input("Enter the choice : "))
+        
+        if(choice == 1):
+            print("\nInserting using Separate Chaining")
+            name = input("Enter name : ")
+            number = int(input("Enter Phone Number : "))
+            d1.setSeparate(name, number)
+            print("\n")
+            print(d1.arrSeperate)
+            print("\n--------------------------------------------------------------------------\n")
+            
+        elif (choice == 2):
+            print("\nInserting using Linear Chaining")
+            name = input("Enter name : ")
+            number = int(input("Enter Phone Number : "))
+            d1.setLinear(name, number)
+            print("\n")
+            print(d1.arrLinear)
+            print("\n--------------------------------------------------------------------------\n")
+            
+        elif(choice == 3):
+            name = input("Enter name : ")
+            print(f"Phone number of {name} is ",d1.lookSeparate(name))
+            print("\n--------------------------------------------------------------------------\n")
+        
+        elif(choice == 4):
+            name = input("Enter name : ")
+            print(f"Phone number of {name} is ",d1.lookLinear(name))
+            print("\n--------------------------------------------------------------------------\n")
+        
+        elif (choice == 5):
             break
-        index = (index + 1) % tab_size
+        else:
+             print("!! Wrong choice entered !!")
 
-    if clients[index] is None:
-        print("Client not found.")
-
-    return comparisons
-
-def look_up_quadratic_probing(x):
-    global clients, tab_size
-    index = hash_function(x)  # Hash Function
-    offset = 1
-    comparisons = 0
-
-    while clients[index] is not None:
-        comparisons += 1
-        if clients[index][0] == x:
-            print(f"{x} client has telephone number as: {clients[index][1]}")
-            break
-        index = (index + offset**2) % tab_size  # Quadratic Probing
-        offset += 1
-
-    if clients[index] is None:
-        print("Client not found.")
-
-    return comparisons
-
-def main_menu():
-    print("1. Insert data in Directory using Linear Probing")
-    print("2. Insert data in Directory using Quadratic Probing")
-    print("3. Look-up for Clients telephone no using Linear Probing")
-    print("4. Look-up for Clients telephone no using Quadratic Probing")
-    print("5. Exit")
-    ch = int(input("Enter your choice: "))
-
-    if ch == 1:
-        print("Inserting data in directory using Linear Probing......")
-        name = input("Enter the name: ")
-        number = int(input("Enter the number: "))
-        insert_linear_probing(name, number)
-        print("-------------------------------------------------------")
-        main_menu()
-    elif ch == 2:
-        print("Inserting data in directory using Quadratic Probing......")
-        name = input("Enter the name: ")
-        number = int(input("Enter the number: "))
-        insert_quadratic_probing(name, number)
-        print("-------------------------------------------------------")
-        main_menu()
-    elif ch == 3:
-        print("Look-up data from directory using Linear Probing......")
-        x = input("Enter the client name for look-up: ")
-        comparisons = look_up_linear_probing(x)
-        print(f"Total comparisons: {comparisons}")
-        print("-------------------------------------------------------")
-        main_menu()
-    elif ch == 4:
-        print("Look-up data from directory using Quadratic Probing......")
-        x = input("Enter the client name for look-up: ")
-        comparisons = look_up_quadratic_probing(x)
-        print(f"Total comparisons: {comparisons}")
-        print("-------------------------------------------------------")
-        main_menu()
-    elif ch == 5:
-        exit()
-    else:
-        print("Enter a valid choice.")
-        main_menu()
-
-print("DSL Assignment no: 1")
-print("Telephone Directory using Hashing")
-main_menu()
+main()
